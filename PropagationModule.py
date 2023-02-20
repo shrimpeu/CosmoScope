1e0842af046593605ef016f2671dd5a3580d3a98
Mark Francis Masadre <masadremarkfrancis@gmail.com>
2023-02-20 22:56:07 +0800

    Added Comments

--- PropagationModule.py
+++ PropagationModule.py
@@ -3,7 +3,11 @@ from matplotlib import *
 from SimulationParameters import * 
 
 
-
+"""
+function that returns the position and velocity of a celestial body
+at a given time (ref_time) from NASA SPICE files. The position and
+velocity are returned as arrays of length 3.
+"""
 def Ephemeris(ref_time,body):
     
     #-------------------------------------------------------------------------
@@ -16,7 +20,13 @@ def Ephemeris(ref_time,body):
     #             index 1 is x,y,z of velocity (np array) of the body obtained 
     #             at time ref_time from NASA SPICE files
     #-------------------------------------------------------------------------
-    
+
+
+    '''
+    obtain the position and velocity of the body from the SPICE files
+    spkezr() returns a tuple whose 0-th element is a state vector.
+    slice the state vector to obtain the position and velocity vectors.
+    '''
     pos_list = spice.spiceypy.spkezr(body,ref_time,'J2000','NONE','SUN')[0][0:3]
     pos_matrix = np.array([ [pos_list[0], pos_list[1],pos_list[2]] ])
     
@@ -26,7 +36,10 @@ def Ephemeris(ref_time,body):
     return (pos_matrix,vel_matrix)
 
 
-
+'''
+function that returns the acceleration of each celestial body in the
+system. The acceleration is returned as an array of length 3.
+'''
 def NbodyProblem(pos, mass):
     
     #-----------------------------------------------------------------
@@ -46,24 +59,28 @@ def NbodyProblem(pos, mass):
     #-----------------------------------------------------------------
     
     global G
-    
+
+    # extract x,y,z coordinates from position matrix
     x = [row[0] for row in pos] # x element of position matrix
     y = [row[1] for row in pos] # y element of position matrix
     z = [row[2] for row in pos] # x element of position matrix
-    acc = np.empty((0,3)) 
-    
+    acc = np.empty((0,3))   # initialize empty array to hold accelerations
+
+    # iterate over each celestial body
     for j in range (0,len(x)):
         ax=0
         ay=0
         az=0
-        
+        # iterate over each other celestial body
         for i in range (0,len(x)):
             if (i!=j):
+                # calculate distance between the two celestial bodies
                 dx = x[i]-x[j]
                 dy = y[i]-y[j]
                 dz = z[i]-z[j]
                 norm = np.sqrt(dx**2+dy**2+dz**2)
-                
+
+                # calculate acceleration due to gravity
                 ax += (G*mass[i]*1e-9*dx)/norm**3
                 ay += (G*mass[i]*1e-9*dy)/norm**3
                 az += (G*mass[i]*1e-9*dz)/norm**3
@@ -89,31 +106,38 @@ def Energy(pos, vel, mass):
     #----------------------------------------------------------------------
     
     global G
-    
+
+    # Extract the x, y, and z elements from the velocity and position matrices
     vx = [row[0] for row in vel] # x element of velocity matrix
     vy = [row[1] for row in vel] # y element of velocity matrix
     vz = [row[2] for row in vel] # z element of velocity matrix 
     x = [row2[0] for row2 in pos] # x element of position matrix
     y = [row2[1] for row2 in pos] # y element of position matrix
     z = [row2[2] for row2 in pos] # z element of position matrix
-    
+
+    # Initialize the kinetic and potential energy variables
     E_kinetic = 0
     E_potential = 0
-    
+
+    # Loop over each particle in the system
     for j in range (0,len(vx)):
               
-        norm = np.sqrt(vx[j]**2+vy[j]**2+vz[j]**2)
-        E_kinetic += (1/2) * mass[j] * norm**2
-        
+        norm = np.sqrt(vx[j]**2+vy[j]**2+vz[j]**2)  # Calculate the speed (magnitude of velocity) of the particle
+        E_kinetic += (1/2) * mass[j] * norm**2  # Calculate the kinetic energy of the particle and add it to the total
+
+        # Loop over all other particles in the system
         for i in range (0,len(x)):
             if (i!=j):
+                # Calculate the distance between particle i and particle j
                 dx = x[i]-x[j]
                 dy = y[i]-y[j]
                 dz = z[i]-z[j]
                 norm2 = np.sqrt(dx**2+dy**2+dz**2)
-                
+
+                # Calculate the potential energy between particle i and particle j and add it to the total
                 E_potential += (G*mass[i]*mass[j]*1e-9)/norm2
-        
+
+    # Return a tuple containing the kinetic and potential energies of the system
     return (E_kinetic,E_potential)
 
 
@@ -141,11 +165,12 @@ def leapfrog(pos0, vel0, mass):
     pos_register = {}
     vel_register = {}
     
-    pos = pos0
-    vel = vel0
-    acc = NbodyProblem(pos,mass)
-    
-    for t in np.arange(t_start, t_end, dt): # kick-drift-kick version
+    pos = pos0  # initial position
+    vel = vel0  # initial velocity
+    acc = NbodyProblem(pos,mass)    # calculate initial acceleration
+
+    # kick-drift-kick version
+    for t in np.arange(t_start, t_end, dt): # iterate over time range with given timestep
 
         vel = vel + acc * dt * 0.5       # kick
         pos = pos + vel * dt             # drift
